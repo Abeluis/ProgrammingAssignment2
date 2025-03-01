0157f0ee6596422b6559456ff6875e14f798bdff
Abel Luis <97851716+Abeluis@users.noreply.github.com>
2025-03-01 00:46:39 -0300

    Update cachematrix.R
    modulo 3

--- cachematrix.R
+++ cachematrix.R
@@ -1,13 +1,50 @@
-# Crear una matriz
-mat <- matrix(c(4, 3, 3, 2), nrow = 2, ncol = 2)
+# makeCacheMatrix: Se crea un objeto "matriz especial" que puede almacenar en caché su inverso.
+makeCacheMatrix <- function(x = matrix()) {
+  # Inicializa la variable para almacenar el inversa como NULL
+  inv <- NULL
+  
+  # Función set: Permite establecer o cambiar la matriz
+  set <- function(y) {
+    x <<- y          # Asigna la nueva matriz
+    inv <<- NULL     # Invalida el caché del inverso cuando la matriz cambia
+  }
+  
+  # Función get: Devuelve la matriz almacenada
+  get <- function() x
+  
+  # Función setInverse: Almacena el inverso calculada en caché
+  setInverse <- function(inverse) inv <<- inverse
+  
+  # Función getInverse: Devuelve el inversa almacenada en caché
+  getInverse <- function() inv
+  
+  # Retorna una lista con las funciones definidas
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
+}
 
-# Crear el objeto "matriz especial"
-cacheMatrix <- makeCacheMatrix(mat)
-
-# Calcular la inversa (no está en caché todavía)
-inverse <- cacheSolve(cacheMatrix)
-print(inverse)
-
-# Obtener la inversa desde la caché (ya está almacenada)
-cachedInverse <- cacheSolve(cacheMatrix)
-print(cachedInverse)
+# cacheSolve: Esta función calcula el inverso de la "matriz especial" creada por makeCacheMatrix.
+# Si el inverso ya ha sido calculado, recupera la inversa de la caché.
+cacheSolve <- function(x, ...) {
+  # Obtiene la inversa almacenada en caché
+  inv <- x$getInverse()
+  
+  # Si el inverso ya está en la caché, lo devuelve y termina la función
+  if (!is.null(inv)) {
+    message("Obteniendo la inversa almacenada en caché")
+    return(inv)
+  }
+  
+  # Si el inverso no está en la caché, obtiene la matriz
+  data <- x$get()
+  
+  # Calcula el inverso usando la función solve
+  inv <- solve(data, ...)
+  
+  # Almacena el inverso calculado en caché
+  x$setInverse(inv)
+  
+  # Devuelve el inverso calculado
+  inv
+}
